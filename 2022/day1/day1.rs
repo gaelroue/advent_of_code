b118bddf31dc434abf0075dde219e8731aff8d1f
Gaël ROUÉ <gaelroue@protonmail.com>
2022-12-07 23:25:49 +0100

    Puzzle day 1

---
+++ 2022/day1/day1.rs
@@ -0,0 +1,25 @@
+use std::fs;
+
+fn part1() -> std::io::Result<()> {
+    let input = fs::read_to_string("input.txt")?;
+    let max = input.split("\n\n").map(|x| x.lines().map(|x| x.parse::<u32>().unwrap()).sum::<u32>()).max().unwrap();
+    println!("The Elf with the most calories carry {} calories.", max);  
+    Ok(())
+}
+
+fn part2() -> std::io::Result<()> {
+    let input = fs::read_to_string("input.txt")?;
+
+    let mut calories = input.split("\n\n").map(|x| x.lines().map(|x| x.parse::<u32>().unwrap()).sum::<u32>()).collect::<Vec<u32>>();
+    calories.sort();
+
+    let ret = calories.into_iter().rev().take(3).sum::<u32>();
+    println!("The top 3 Elves carrying the most calories have a total of {} calories.", ret);   
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    part1()?;
+    part2()?;
+    Ok(())
+}
